a461ed0adce66977381f6c98ea7256475bcb9a91
Valerija Schulz <135040463+zzzVSchulzzz@users.noreply.github.com>
2024-04-15 10:23:20 +0300

    try localeCompare()

--- task 1/script.js
+++ task 1/script.js
@@ -27,3 +27,23 @@ const match2 = function match2(str1, str2){
 }
 
 match2('helLo', 'HEllo')
+
+/*
+    Использование метода localeCompare()
+
+    Ещё один способ — использовать метод localeCompare(), 
+    который сравнивает строки с учетом локали и может не учитывать регистр. 
+    Для этого достаточно передать в метод параметр {sensitivity: 'base'}.
+ */
+
+
+const match3 = function match3(str1, str2){
+    if(str1.localeCompare(str2, undefined, {sensitivity: 'base'}) == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+match3('helLo', 'HEllo')
+
